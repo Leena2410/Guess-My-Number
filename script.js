ec5b176de36088d0f5f12d36c82e6df15114ecf4
unknown <emadeldeen.lina@gmail.com>
2023-05-17 14:20:28 +0300

    17th May

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 "use strict";
 
 console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "Correct Number";
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = document.querySelector(".guess").value;
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number";
+  } else {
+    document.querySelector(".message").textContent = `The Guess is ${guess}`;
+  }
+});
+
+function check() {
+  console.log(document.querySelector(".guess").value);
+}
