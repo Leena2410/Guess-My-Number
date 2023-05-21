4fcf2fd9e2c3e925776609f2023a5e509e8bc0c1
unknown <emadeldeen.lina@gmail.com>
2023-05-21 16:49:34 +0300

    Edit Problems - Minus score - Red background - 'close' system - using enter to submit guess

--- script.js
+++ script.js
@@ -9,64 +9,46 @@ console.log(secret);
 document.querySelector(".check").addEventListener("click", function () {
   const guess = document.querySelector(".guess").value;
   if (!guess) {
-    document.querySelector(".message").textContent = "No Number";
-  } else if (guess == secret) {
-    /*----------*/
-    document.querySelector("body").style.backgroundColor = "#60b347";
-    document.querySelector(".number").style.width = "30rm";
-    document.querySelector(".number").textContent = secret;
-    document.querySelector(".message").textContent = "Right Number";
-    document.querySelector(".hint").disabled = true;
-    document.querySelector(".check").disabled = true;
-    document.querySelector(".hint").style.backgroundColor = "#f0f0f0";
-    document.querySelector(".check").style.backgroundColor = "#f0f0f0";
-    if (score > 5) {
-      hint++;
-      document.querySelector(".HintMessage").textContent = `Hints = ${hint}`;
+    displyMessage("No Number");
+  } else {
+    if (guess == secret) {
+      document.querySelector("body").style.backgroundColor = "#60b347";
+      viewSecret();
+      displyMessage("Right Number");
+      disable();
+      if (score > 5) {
+        hint++;
+        document.querySelector(".HintMessage").textContent = `Hints = ${hint}`;
+      }
+      if (score > best_score) {
+        best_score = score;
+        document.querySelector(".highscore").textContent = best_score;
+      }
+    } else if (guess > secret + 2) {
+      displyMessage("Too Heigh");
+      scoreMin();
+    } else if (guess < secret - 2) {
+      displyMessage("Too Low");
+      scoreMin();
+    } else {
+      displyMessage("Close...?");
+      scoreMin();
     }
-
-    if (score > best_score) {
-      best_score = score;
-      document.querySelector(".highscore").textContent = best_score;
-    }
-  } else if (guess > secret) {
-    wrongGuess();
-    displyMessage("Too Heigh");
-  } else if (guess < secret) {
-    wrongGuess();
-    displyMessage("Too Low");
   }
 });
 
 document.querySelector(".again").addEventListener("click", function () {
-  document.querySelector(".message").textContent = "Start Guessing...";
+  displyMessage("Start Guessing...");
   document.querySelector(".number").textContent = "?";
   document.querySelector("body").style.backgroundColor = "#000000";
   score = 10;
   document.querySelector(".label-score").textContent = `💯 Score: ${score}`;
   secret = randomNum();
   document.querySelector(".guess").value = "";
-  document.querySelector(".hint").disabled = false;
-  document.querySelector(".check").disabled = false;
-  document.querySelector(".hint").style.backgroundColor = "#eee";
-  document.querySelector(".check").style.backgroundColor = "#eee";
+  enable();
 
   console.log(secret);
 });
-
-function wrongGuess() {
-  if (score > 1) {
-    document.querySelector("body").style.backgroundColor = "#ff0000";
-    document.querySelector(".number").style.width = "30rm";
-    score--;
-    document.querySelector(".label-score").textContent = `💯 Score: ${score}`;
-  } else {
-    score--;
-    document.querySelector(".label-score").textContent = `💯 Score: ${score}`;
-    displyMessage("You Lost The Game");
-    document.querySelector(".number").textContent = secret;
-  }
-}
 
 function displyMessage(message) {
   document.querySelector(".message").textContent = message;
@@ -75,27 +57,50 @@ function randomNum() {
   return Math.floor(Math.random() * 20 + 1);
 }
 
-function check(num) {}
 document.querySelector(".hint").addEventListener("click", function () {
-  let h;
+  let hintValue = 0;
   if (hint > 0) {
+    do {
+      hintValue = randomNum();
+    } while (hintValue == secret);
+
+    if (hintValue < secret) {
+      displyMessage(`The Number Is Bigger Than ${hintValue}`);
+    } else {
+      displyMessage(`The Number Is Smaller Than ${hintValue}`);
+    }
+
     hint--;
     document.querySelector(".HintMessage").textContent = `Hints = ${hint}`;
-    do {
-      h = randomNum();
-    } while (h == secret);
-
-    if (secret > h) {
-      document.querySelector(
-        ".message"
-      ).textContent = `The Number Is Bigger Than ${h}`;
-    } else {
-      document.querySelector(
-        ".message"
-      ).textContent = `The Number Is Smaller Than ${h}`;
-    }
   } else {
-    document.querySelector(".message").textContent =
-      "You Dont Have Any Hint Points";
+    displyMessage("You Have No Hint Points");
   }
 });
+
+function scoreMin() {
+  score--;
+  document.querySelector(".label-score").textContent = `💯 Score: ${score}`;
+  if (score == 0) {
+    displyMessage("You Lost The Game");
+    disable();
+  }
+}
+
+function disable() {
+  document.querySelector(".hint").disabled = true;
+  document.querySelector(".check").disabled = true;
+  document.querySelector(".hint").style.backgroundColor = "#f0f0f0";
+  document.querySelector(".check").style.backgroundColor = "#f0f0f0";
+}
+
+function viewSecret() {
+  document.querySelector(".number").style.width = "30rm";
+  document.querySelector(".number").textContent = secret;
+}
+
+function enable() {
+  document.querySelector(".hint").disabled = false;
+  document.querySelector(".check").disabled = false;
+  document.querySelector(".hint").style.backgroundColor = "#eee";
+  document.querySelector(".check").style.backgroundColor = "#eee";
+}
